6dd01494b0cd1b217588333a19bdeca222846fbe
蔡泽坚 <wb-czj380013@alibaba-inc.com>
2018-11-02 16:46:08 +0800

    release

--- frameworks/AliyunVideoSDKPro.framework/Headers/AliyunClip.h
+++ frameworks/AliyunVideoSDKPro.framework/Headers/AliyunClip.h
@@ -9,36 +9,46 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "AliyunJSONModel.h"
+#import "AliyunAction.h"
+#import "AliyunTransitionEffect.h"
+
 
 
 @protocol AliyunClip;
 typedef NS_ENUM(NSInteger,AliyunClipType) {
-    AliyunClipVideo,
-    AliyunClipImage
+    AliyunClipVideo = 0,
+    AliyunClipImage = 1,
+    AliyunClipAudio = 2,
+    AliyunClipGif = 3
 };
 
 @interface AliyunClip : AliyunJSONModel
 /*
- mediaType  :  类型，视频或图片image，类型改为0,1 0标示视频，1标示图片  
- mediaHeight:  图片/视频／图片的分辨率
- mediaWidth :
- src        :  
- startTime  :  图片/视频、配音 开始时间
- endTime    :  图片/视频、配音 结束时间
- rotation   :  图片/视频角度
- duration   :  图片
+ mediaType    :  类型，视频或图片image，类型改为0,1 0标示视频，1标示图片
+ mediaHeight  :  图片/视频高
+ mediaWidth   :  图片/视频宽
+ src          :  资源路径
+ startTime    :  视频/配音 开始时间
+ fadeDuration :  过渡时长
+ displayMode  :  显示模式 0:截断填充 1:黑边填充
+ rotation     :  图片/视频角度
+ duration     :  图片/视频/配音时长
+ streamId     :  图片/视频/配音流id，sdk内部生成，请勿直接赋值
+ transitionEffect: 转场效果 是与前一段视频段进行转场
+ recordStartTime: 音频录制开始时间，在播放轴上的时间
  */
 @property (nonatomic, assign) AliyunClipType mediaType;
 @property (nonatomic, assign) int mediaHeight;
 @property (nonatomic, assign) int mediaWidth;
 @property (nonatomic, copy) NSString *src;
 @property (nonatomic, assign) CGFloat startTime;
-@property (nonatomic, assign) CGFloat endTime;
 @property (nonatomic, assign) CGFloat fadeDuration;
 @property (nonatomic, assign) int displayMode;
 @property (nonatomic, assign) int rotation;
 @property (nonatomic, assign) CGFloat duration;
 @property (nonatomic, assign) int streamId;
+@property (nonatomic, strong) AliyunTransitionEffect *transitionEffect;
+@property (nonatomic, assign) CGFloat audioRecordStartTime;
 
 /**
  创建一个图片片段
@@ -54,6 +64,28 @@ typedef NS_ENUM(NSInteger,AliyunClipType) {
 
 
 /**
+ 创建一个gif片段
+
+ @param path gif路径
+ @return gif片段
+ */
+- (instancetype)initWithGifPath:(NSString *)path;
+
+
+/**
+ 创建一个gif片段
+
+ @param path gif路径
+ @param startTime 指定gif的开始播放时间
+ @param duration 指定gif的持续播放时间
+ @return gif片段
+ */
+- (instancetype)initWithGifPath:(NSString *)path
+                      startTime:(CGFloat)startTime
+                       duration:(CGFloat)duration;
+
+
+/**
  创建一个视频片段
 
  @param path 视频路径
@@ -62,6 +94,8 @@ typedef NS_ENUM(NSInteger,AliyunClipType) {
  */
 - (instancetype)initWithVideoPath:(NSString *)path
                      animDuration:(CGFloat)animDuration;
+
+
 
 
 /**
@@ -77,6 +111,35 @@ typedef NS_ENUM(NSInteger,AliyunClipType) {
                         startTime:(CGFloat)startTime
                          duration:(CGFloat)duration
                      animDuration:(CGFloat)animDuration;
+
+
+/**
+ API_AVAILABLE(3.7.0)
+ 
+ 添加动画 注意：主流不支持alpha帧动画
+
+ @param action 动画
+ */
+- (void)runAction:(AliyunAction *)action;
+
+/**
+ API_AVAILABLE(3.7.0)
+ 
+ 停止动画
+
+ @param action 动画
+ */
+- (void)stopAction:(AliyunAction *)action;
+
+
+/**
+ API_AVAILABLE(3.7.0)
+ 
+ 获取所有的动画
+
+ @return 动画数组
+ */
+- (NSArray *)allActions;
 
 @end
 
